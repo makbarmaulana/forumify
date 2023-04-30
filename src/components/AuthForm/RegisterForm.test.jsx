e23af122d5c3d804c774cf9e2e75b1975de93e7f
M Akbar Maulana <makbarm.id@gmail.com>
2023-05-01 00:08:35 +0700

    Fix: Testing Compoenents (LoginForm, RegisterForm, Leaderboards)

--- src/components/AuthForm/RegisterForm.test.jsx
+++ src/components/AuthForm/RegisterForm.test.jsx
@@ -9,7 +9,7 @@
  */
 
 import React from 'react';
-import { act, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import {
   describe, expect, it, vi,
 } from 'vitest';
@@ -19,7 +19,7 @@ import RegisterForm from './RegisterForm';
 
 function WrappedRegisterForm({ register }) {
   return (
-    <MemoryRouter>
+    <MemoryRouter initialEntries={['/register']}>
       <RegisterForm register={register} />
     </MemoryRouter>
   );
@@ -27,32 +27,30 @@ function WrappedRegisterForm({ register }) {
 
 describe('LoginInput component', () => {
   it('should handle username typing correctly', async () => {
-    await act(async () => render(<WrappedRegisterForm />));
+    render(<WrappedRegisterForm />);
     const usernameField = screen.getByLabelText('Username');
 
-    await act(async () => userEvent.type(usernameField, 'User Testing'));
+    await userEvent.type(usernameField, 'User Testing');
 
     expect(usernameField).toHaveValue('User Testing');
   });
 
   it('should handle email typing correctly', async () => {
-    await act(async () => render(<WrappedRegisterForm />));
+    render(<WrappedRegisterForm />);
     const emailField = screen.getByLabelText('Email Address');
 
-    await act(async () => userEvent.type(emailField, 'usertesting@mail.com'));
+    await userEvent.type(emailField, 'usertesting@mail.com');
 
     expect(emailField).toHaveValue('usertesting@mail.com');
   });
 
   it('should handle password and confirm password typing correctly', async () => {
-    await act(async () => render(<WrappedRegisterForm />));
+    render(<WrappedRegisterForm />);
     const passwordField = screen.getByLabelText('Password');
     const confirmPasswordField = screen.getByLabelText('Confirm Password');
 
-    await act(async () => {
-      await userEvent.type(passwordField, 'usertesting');
-      await userEvent.type(confirmPasswordField, 'usertesting');
-    });
+    await userEvent.type(passwordField, 'usertesting');
+    await userEvent.type(confirmPasswordField, 'usertesting');
 
     expect(passwordField).toHaveValue('usertesting');
     expect(confirmPasswordField).toHaveValue('usertesting');
@@ -61,20 +59,18 @@ describe('LoginInput component', () => {
 
   it('should call register function when register button is clicked', async () => {
     const mockRegister = vi.fn();
-    await act(async () => render(<WrappedRegisterForm register={mockRegister} />));
+    render(<WrappedRegisterForm register={mockRegister} />);
     const usernameField = screen.getByLabelText('Username');
     const emailField = screen.getByLabelText('Email Address');
     const passwordField = screen.getByLabelText('Password');
     const confirmPasswordField = screen.getByLabelText('Confirm Password');
     const loginButton = screen.getByRole('button', { name: 'Register' });
 
-    await act(async () => {
-      await userEvent.type(usernameField, 'User Testing');
-      await userEvent.type(emailField, 'usertesting@mail.com');
-      await userEvent.type(passwordField, 'usertesting');
-      await userEvent.type(confirmPasswordField, 'usertesting');
-      await userEvent.click(loginButton);
-    });
+    await userEvent.type(usernameField, 'User Testing');
+    await userEvent.type(emailField, 'usertesting@mail.com');
+    await userEvent.type(passwordField, 'usertesting');
+    await userEvent.type(confirmPasswordField, 'usertesting');
+    await userEvent.click(loginButton);
 
     expect(confirmPasswordField.value).toEqual(passwordField.value);
     expect(mockRegister).toBeCalledWith({
